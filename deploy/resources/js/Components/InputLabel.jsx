ea85761d757e58a228eeb8b3195c7cc1e4596787
carlos <olavera93@gmail.com>
2026-07-14 22:14:43 -0500

    limpiando el repositorio

--- deploy/resources/js/Components/InputLabel.jsx
+++
@@ -1,7 +0,0 @@
-export default function InputLabel({ value, className = '', children, ...props }) {
-    return (
-        <label {...props} className={`block text-[10px] font-black text-slate-500 dark:text-slate-400 uppercase tracking-widest mb-1 ml-1 ` + className}>
-            {value ? value : children}
-        </label>
-    );
-}
